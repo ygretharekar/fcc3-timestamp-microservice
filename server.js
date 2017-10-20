bf61bef1ac3a09f575ab440eeefa0dab9c63cf6f
Glitch (scrawny-scorpion) <none>
2017-10-20 22:36:19 +0000

    Exporting to fcc3-timestamp-microservice

--- server.js
+++ server.js
@@ -7,20 +7,9 @@
 
 var fs = require('fs');
 var express = require('express');
+var moment = require('moment');
+//var path = require('path');
 var app = express();
-
-if (!process.env.DISABLE_XORIGIN) {
-  app.use(function(req, res, next) {
-    var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
-    var origin = req.headers.origin || '*';
-    if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
-         console.log(origin);
-         res.setHeader('Access-Control-Allow-Origin', origin);
-         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    }
-    next();
-  });
-}
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -36,12 +25,43 @@ app.route('/_api/package.json')
 app.route('/')
     .get(function(req, res) {
 		  res.sendFile(process.cwd() + '/views/index.html');
-    })
+    });
+
+app.route('/:string')
+   .get(
+     function(req, res) {
+       
+       
+       
+       res.json({
+        unix: 'hello',
+        natural: 'world'
+       });
+    });
+
+app.listen(process.env.PORT, function () {
+  console.log('Node.js listening ...');
+});
+
+
+/*
+if (!process.env.DISABLE_XORIGIN) {
+  app.use(function(req, res, next) {
+    var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
+    var origin = req.headers.origin || '*';
+    if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
+         console.log(origin);
+         res.setHeader('Access-Control-Allow-Origin', origin);
+         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    }
+    next();
+  });
+}
 
 // Respond not found to all the wrong routes
 app.use(function(req, res, next){
   res.status(404);
-  res.type('txt').send('Not found');
+  res.type('txt').send('Page Not found');
 });
 
 // Error Middleware
@@ -53,7 +73,7 @@ app.use(function(err, req, res, next) {
   }  
 })
 
-app.listen(process.env.PORT, function () {
-  console.log('Node.js listening ...');
-});
+*/
+
+
 
